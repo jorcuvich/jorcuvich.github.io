7e5df8694702a75e1583c8b17a3d31e93f1495b0
jhotiori <jho.martiori@gmail.com>
2025-09-18 10:54:54 -0300

    refactor(dev): include 'vite.config.js' configuration file

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+/*
+    Vite Dev-server configuration file for local development.
+    @author jhotiori
+*/
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    server: {
+        host: "localhost",
+        port: 8080
+    }
+})
